bb269a104b28bdcb78fbf2302b584145617bc82c
Irene Spezzamonte <irenespezzamonte@MacBook-Pro-2.local>
2018-03-15 13:23:58 -0400

    first add

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	var
+		grant2018: 1, 000
+	language head: english
+	city: new york city
+	ngo: ["helpUnlimited", "newStart", "betterTomorrow", "homeSafe", "nowHome"]
+	monthlyExpenses = {
+		family: ["onePax", "couple", "fam3", "fam4", "fam5orMore"]
+		expenses: [800, 950, 1000, 1100, 1200]
+	}
+});
